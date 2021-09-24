df457895923a0e76fcba14c768ebca22716d230a
Nam Wook Kim <namwkim85@gmail.com>
2021-09-24 16:32:20 -0400

    update home page

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -7,33 +7,38 @@ import Grid from "@material-ui/core/Grid";
 import { Link } from "react-router-dom";
 function Home() {
     return (
-        <Grid container spacing={3} justify="center">
-            <Grid item xs={6}>
-                <Typography variant="h2" gutterBottom>
-                    KORI
-                </Typography>
-                <Box marginBottom={2}>
-                    <Typography variant="body1" gutterBottom>
-                        Abstract
-                    </Typography>
-                </Box>
-                <Fab
-                    variant="extended"
-                    color="primary"
-                    aria-label="add"
-                    component={Link}
-                    to="/docs"
-                >
-                    <NavigationIcon />
-                    Get Started
-                </Fab>
+        <React.Fragment>
+            <Typography variant="h2" gutterBottom>
+                KORI
+            </Typography>
+            <Grid container spacing={3} justify="center">
+                <Grid item xs={6}>
+
+                    <Box marginBottom={2}>
+                        <Typography variant="body1" gutterBottom>
+                            Charts go hand in hand with text to communicate complex data and are widely adopted in news articles, online blogs, and academic papers. They provide graphical summaries of the data, while text explains the message and context. However, synthesizing information across text and charts is difficult; it requires readers to frequently shift their attention. We investigated ways to support the tight coupling of text and charts in data documents. To understand their interplay, we analyzed the design space of chart-text references through news articles and scientific papers. Informed by the analysis, we developed a mixed-initiative interface enabling users to construct interactive references between text and charts. It leverages natural language processing to automatically suggest references as well as allows users to manually construct other references effortlessly. A user study complemented with algorithmic evaluation of the system suggests that the interface provides an effective way to compose interactive data documents.
+                        </Typography>
+                    </Box>
+                    <Fab
+                        variant="extended"
+                        color="primary"
+                        aria-label="add"
+                        component={Link}
+                        to="/docs"
+                    >
+                        <NavigationIcon />
+                        Get Started
+                    </Fab>
+                </Grid>
+                <Grid item xs={6} style={{overflow:"hidden"}}>
+                    <video width="110%" height="auto" autoPlay style={{marginLeft:"-5%"}}>
+                        <source src="teaser.mp4" type="video/mp4" />
+                    </video>
+
+                </Grid>
             </Grid>
-            <Grid item xs={6}>
-                <Typography variant="overline" display="block" gutterBottom>
-                    Teaser Image/Video
-                </Typography>
-            </Grid>
-        </Grid>
+        </React.Fragment>
+
     );
 }
 
